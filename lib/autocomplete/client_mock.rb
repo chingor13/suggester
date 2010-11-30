78936fbe8078bb7324e10adc10dd4d9519621639
Jeff Ching <ching.jeff@gmail.com>
2010-11-30 11:53:49 -0800

    adding client mock for testing

---
+++ lib/autocomplete/client_mock.rb
@@ -0,0 +1,53 @@
+module Autocomplete
+  class ClientMock
+    def initialize(options = {})
+      @mocks = {}
+    end
+
+    def match(handler, query, options = {})
+      get_test_response_results("match", handler, query, opts)
+    end
+
+    def find(handler, query, options = {})
+      get_test_response_results("find", handler, query, opts)
+    end
+
+    def refresh
+      true
+    end
+
+    def set_test_results(data, type, handler, query, opts = {})
+      opts[:query] = query
+      key = "#{type}--#{handler}"
+      @mocks[key] ||= []
+      @mocks[key] << {:opts => opts, :buffer => data}
+    end
+
+    def clear_test_results
+      @mocks = {}
+    end
+
+    private
+
+    def get_test_response_results(type, handler, opts)
+      key = "#{type}--#{handler}"
+      match = @mocks[key].and.detect do |m|
+        options_match?(m[:opts], opts)
+      end
+      match.and[:buffer] || []
+    end
+
+    def options_match?(opts1, opts2)
+      opts1.each do |k,v|
+        unless opts2.keys.include?(k)
+          return false
+        end
+        unless opts2[k] == v
+          return false
+        end
+      end
+      true
+    end
+
+  end
+end
